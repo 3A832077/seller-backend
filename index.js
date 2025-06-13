fbc7742c69017c095ba5bbee0f3641f962ca82c7
shu <a.0066715@gmail.com>
2025-06-13 12:04:05 +0800

    修改配置

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const jsonServer = require('json-server');
+// api/index.js
+import jsonServer from 'json-server';
+import path from 'path';
+
+// 建立 JSON Server
 const server = jsonServer.create();
-const path = require('path');
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const router = jsonServer.router(path.join(process.cwd(), 'db.json')); // 注意這裡
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-
-// 可新增自定義路由或邏輯
 server.use(router);
 
-module.exports = server;
+// 匯出符合 Vercel serverless function 規範的 handler
+export default server;
