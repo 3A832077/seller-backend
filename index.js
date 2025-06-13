ffbaddee8b31bfdb17891ff67072c1c8f0c116c6
shu <a.0066715@gmail.com>
2025-06-13 14:43:18 +0800

    json server 降到0.17.4版

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// api/index.js
-import jsonServer from 'json-server';
-import path from 'path';
+// api/server.js
+const jsonServer = require('json-server');
+const path = require('path');
 
-// 建立 JSON Server
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), 'db.json')); // 注意這裡
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
 server.use(router);
 
-// 匯出符合 Vercel serverless function 規範的 handler
-export default server;
+module.exports = server;
