175900c3f577a2cf6f12589fce5a941f52bd7214
shu <a.0066715@gmail.com>
2025-06-16 10:15:09 +0800

    修改

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'json-server', 'db.json'));
-const middlewares = jsonServer.defaults();
 
+const router = jsonServer.router(path.join(__dirname, '..', 'json-server', 'db.json'));
+
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
