c663ef7d92f6996abb39b299b5d946df0df61c17
shu <a.0066715@gmail.com>
2025-06-16 10:08:07 +0800

    index.js 改位置

--- src/app/env/environment.prod.ts
+++ src/app/env/environment.prod.ts
@@ -1,7 +1,7 @@
 // 生產環境
 export const env = {
   production: true,
-  apiUrl: 'https://seller-backend-two.vercel.app',
+  apiUrl: 'https://seller-backend-two.vercel.app/api',
   googleClientId: '584420335213-h91mg7sv5t6tde43k0bccdodj2k3dvud.apps.googleusercontent.com',
   googleApiKey: 'AIzaSyA7xQNhmBqfJGW6nfuwzovPaEbJJ5lsJt0',
   googleApiUrl: 'https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1',
